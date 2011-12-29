bb0d537c1b42386bb5cefdc2b91a8fbc49675138
Diego Stamigni <diego.stamigni@gmail.com>
2011-12-29 14:44:07 +0100

    first impression of ereditariety

---
+++ src/it/areamobile/apis/hw/areafly/entity/Discoverer.java
@@ -0,0 +1,8 @@
+package it.areamobile.apis.hw.areafly.entity;
+
+/**
+ * Created by AreaMobile
+ * Date: 29/12/11
+ */
+
+public class Discoverer extends Thread {}
--- src/it/areamobile/apis/hw/areafly/entity/FlyPort.java
+++ src/it/areamobile/apis/hw/areafly/entity/FlyPort.java
@@ -4,4 +4,56 @@ package it.areamobile.apis.hw.areafly.entity;
  * Created by AreaMobile
  * Date: 28/12/11
  */
-public class FlyPort {}
+public abstract class FlyPort extends Warrior implements Comparable {
+    private String mac_address;
+    private String ip_address;
+    private int port;
+    private String netbios_name;
+
+    public FlyPort() {};
+
+    @Override
+    public String toString() {
+        return null;
+    }
+
+    @Override
+    public String getMacAddress() {
+        return mac_address;
+    }
+
+    @Override
+    public void setMacAddress(String mac_address) {
+        this.mac_address = mac_address;
+    }
+
+    @Override
+    public String getIp_address() {
+        return ip_address;
+    }
+
+    @Override
+    public void setIPAddress(String ip_address) {
+        this.ip_address = ip_address;
+    }
+
+    @Override
+    public int getPort() {
+        return port;
+    }
+
+    @Override
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    @Override
+    public String getNetBiosName() {
+        return netbios_name;
+    }
+
+    @Override
+    public void setNetBiosName(String netbios_name) {
+        this.netbios_name = netbios_name;
+    }
+};
